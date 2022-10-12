0e8caa4d8e25cbef2b07a1ccdda83237217d3c87
Timur Ermolaev <timkogas@gmail.com>
2022-10-12 11:27:24 +0300

    Back - реализовал вывод сообщений для всех пользователей

--- homework_95_back/index.js
+++ homework_95_back/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const users = require("./routes/users");
+const {nanoid} = require("nanoid")
 const app = express();
-const enableWs = require("express-ws")
+const enableWs = require("express-ws");
+const auth = require("./middleware/auth");
+const User = require("./models/User");
 enableWs(app)
 
 const port = 8000;
@@ -14,9 +17,39 @@ app.use("/users", users);
 
 const activeConnections = {}
 
-app.ws('/chat', (ws, req)=> {
+app.ws('/chat', async (ws, req)=> {
   const id = nanoid()
+  let query
+  let user
+  if (req.query.token) {
+    query = req.query.token
+    user = await User.findOne({token: query})
+  }
   activeConnections[id] = ws
+  ws.on('message', (message) => {
+    const decodedMessage = JSON.parse(message)
+    switch(decodedMessage.type) {
+      case "MESSAGE_CREATED": 
+        if (user) {
+          Object.keys(activeConnections).forEach(connId=>{
+            const conn = activeConnections[connId]
+            conn.send(JSON.stringify({
+              type: "NEW_MESSAGE",
+              message: {
+                username: user.username,
+                text: decodedMessage.message
+              }
+            }))
+          })
+        } else {
+          ws.send('Login')
+        }
+        break;
+      default:
+        console.log('Unknown message type ' + decodedMessage.type)
+        break;
+    }
+  })
 
   ws.on('close', (msg)=>{
     delete activeConnections[id]
